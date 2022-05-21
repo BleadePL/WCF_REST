2f8c58cf934dc235725ee37b28d323e0b3fbcf48
Konrad <konrad.wieckiewicz@gmail.com>
2022-05-21 12:59:59 +0200

    Add project files.

---
+++ WCF_REST/IRestService.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCF_REST
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IRestService
+    {
+        //XML
+        [OperationContract]
+        [WebGet(UriTemplate = "/Manga")]
+        List<Manga> GetAllMangas();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/Manga/{id}",
+                ResponseFormat = WebMessageFormat.Xml)]
+        Manga GetByIdXml(string id);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/Add",
+                   Method = "POST",
+                   ResponseFormat = WebMessageFormat.Xml)]
+        string addXml(Manga manga);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/Manga{id}", Method = "DELETE")]
+        string deleteXml(string id);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/MangaEdit{id}", Method = "PUT")]
+        string editXml(string id, Manga manga);
+
+
+/*        //JSON
+        [OperationContract]
+        [WebGet(UriTemplate = "/json/Manga")]
+        List<Manga> GetAllMangasJson();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/Manga/{id}",
+                ResponseFormat = WebMessageFormat.Json)]
+        Manga GetByIdJson(string id);
+
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/json/Add",
+                   Method = "POST",
+                   ResponseFormat = WebMessageFormat.Json)]
+        string addJson(Manga manga);
+
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/json/Manga{id}", Method = "DELETE")]
+        string deleteJson(string id);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "/json/MangaEdit{id}", Method = "PUT")]
+        string editJson(string id, Manga manga);*/
+
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class Manga
+    {
+/*        string mangaTitle = "default";
+        string mangaAuthor = "default author";
+        int mangaRate = 4;*/
+
+        [DataMember]
+        public int? Id { get; set; }
+
+        [DataMember(Order = 0)]
+        public string MangaTitle { get; set; }
+
+        [DataMember(Order = 1)]
+        public string MangaAuthor { get; set; }
+
+        [DataMember(Order = 2)]
+        public int MangaRate { get; set; }
+
+    }
+}
---
+++ WCF_REST/MyRestService.svc.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCF_REST
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class MyRestService : IRestService
+    {
+        private static Dictionary<int, Manga> Mangas =
+            new Dictionary<int, Manga>()
+            {
+                { 1, new Manga {Id = 1, MangaTitle = "Assasination Classroom", MangaAuthor = "Noriko Ozaki", MangaRate = 5 } },
+                { 2, new Manga {Id = 2, MangaTitle = "Kakegurui", MangaAuthor = "Homura Kawamoto", MangaRate = 5}},
+                { 3, new Manga {Id = 3, MangaTitle = "Tokyo Ghoul", MangaAuthor = "Sui Ishida", MangaRate = 4}}
+            };
+
+        public string addXml(Manga manga)
+        {
+            if (manga == null)
+            {
+                throw new WebFaultException<string>("400: Incorrect Data",
+                    System.Net.HttpStatusCode.BadRequest);
+            }
+
+            int count = Mangas.Values.Where(m => m.MangaTitle.Equals(m.MangaTitle) && 
+                                                      m.MangaAuthor.Equals(manga.MangaAuthor) && 
+                                                      m.MangaRate == manga.MangaRate).Count();
+
+            if (count > 0)
+            {
+                throw new WebFaultException<string>("409: The input Data Exists in the Database",
+                    System.Net.HttpStatusCode.Conflict);
+            }
+            else
+            {
+                int id = Mangas.Keys.ToList().LastOrDefault() + 1;
+                manga.Id = id;
+
+                Mangas.Add(id, manga);
+            }
+            return $"Added element Successfully on {manga.Id} id";
+        }
+
+        public string deleteXml(string id)
+        {
+            int Id = int.Parse(id);
+
+            if (Mangas.ContainsKey(Id))
+            {
+                Mangas.Remove(Id);
+                return $"Successfully removed element at {Id} id";
+            }
+            else
+            {
+                throw new WebFaultException<string>("404: Element Not Found",
+                                    System.Net.HttpStatusCode.NotFound);
+            }
+        }
+
+        public string editXml(string id, Manga manga)
+        {
+            int Id = int.Parse(id);
+            if (Mangas.ContainsKey(Id) && manga != null)
+            {
+                var mangaToEdit = Mangas[Id];
+
+                mangaToEdit.MangaTitle = manga.MangaTitle;
+                mangaToEdit.MangaAuthor = manga.MangaAuthor;
+                mangaToEdit.MangaRate = manga.MangaRate;
+
+                return $"Successfully Edited element on {Id} id";
+            }
+            else
+            {
+                throw new WebFaultException<string>("404: Element Not Found",
+                                                    System.Net.HttpStatusCode.NotFound);
+            }
+        }
+
+        public List<Manga> GetAllMangas()
+        {
+           return Mangas.Values.ToList();
+        }
+
+        public Manga GetByIdXml(string id)
+        {
+            int Id = int.Parse(id);
+            if (Mangas.ContainsKey(Id))
+            {
+                return Mangas[Id];
+            }
+            else
+            {
+                throw new WebFaultException<string>("404: Element Not Found",
+                                                    System.Net.HttpStatusCode.NotFound);
+            }
+        }
+    }
+}
